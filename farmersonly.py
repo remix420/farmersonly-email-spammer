38cf554ec6f65a3ff0a15a90baace5151811669d
remix <72325094+remixpy@users.noreply.github.com>
2021-01-12 18:45:32 -0600

    Create farmersonly.py

---
+++ farmersonly.py
@@ -0,0 +1,40 @@
+import requests
+import random
+import os
+import ctypes
+
+email_username = '' # the part of the email before '@gmail.com'
+count = 1
+os.system('mode con: cols=75 lines=20')
+ctypes.windll.kernel32.SetConsoleTitleW("FarmersOnly Email Spammer")
+
+
+
+def get_num():
+	global num
+	global age
+	global zipc
+	num = '+' + str(random.randint(1000000000000,99999999999999999))
+	age = str(random.randint(21,50))
+	zipc = ''
+def doit():
+	global count
+	get_num()
+	payload = {
+		'registration[minAge]': '18',
+		'registration[maxAge]': '99',
+		'registration[email]': email_username + '{}@gmail.com'.format(num),
+		'registration[zipcode]': zipc,
+		'registration[genders]': 'MF',
+		'registration[age]': age,
+		'registration[acceptTerms]': '0',
+		'registration[acceptTerms]': '1',
+		'x': '221',
+		'y': '44'
+		}
+	requests.post('https://www.farmersonly.com/', data=payload)
+	os.system('cls')
+	print('sent ' + str(count) +  ' emails')
+	count += 1
+while True:
+ doit()
